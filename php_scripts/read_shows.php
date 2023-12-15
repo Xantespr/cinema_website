f07a5c5c3cd4377bd6567529df480bc72a95a6c6
Xantespr <45178757+Xantespr@users.noreply.github.com>
2023-12-16 00:02:38 +0100

    Add files via upload

---
+++ php_scripts/read_shows.php
@@ -0,0 +1,24 @@
+<?php 
+    $c = mysqli_connect("localhost", "root", "", "cinema_db");
+    
+    if ($c->connect_error) {
+        die("Connection failed: " . $c->connect_error);
+    }
+
+    $selectedDay = $_POST['day'];
+
+    $query = "SELECT showing.id, film.title FROM `showing` JOIN film on film.id = showing.film_id WHERE DATE(`date`) = '$selectedDay' ORDER BY showing.date";
+
+    $result = mysqli_query($c, $query);
+
+    $films_data = array();
+
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        $films_data[] = $row;
+    }
+
+    echo json_encode($films_data);
+    
+    mysqli_close($c);
+?>
